9ec49a9a6a044598592da58bccc4c015a51f25e9
Joe Liang <56658669+1000mileworld@users.noreply.github.com>
2020-03-21 19:02:44 -0400

    profile image responsive

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// $(document).ready(function(){
+//     let navHeight = $("nav").height();
+//     //console.log(navHeight);
+//     let 
+//     $("header").css({height: `calc(100vh-${navHeight})`})
+// });
