dd82ef9ce6ef9a193613eacb411f894b0d8c62a9
Joe Liang <56658669+1000mileworld@users.noreply.github.com>
2020-03-21 16:32:30 -0400

    reusable nav across multiple pages

---
+++ nav.js
@@ -0,0 +1,25 @@
+
+document.getElementById('nav-placeholder').outerHTML=`
+
+
+<nav class="navbar navbar-expand-md">
+    <a class="navbar-brand" href="#">Logo</a>
+    <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="main-navigation">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="index.html">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="projects.html">Projects</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact.html">Contact</a>
+        </li>
+      </ul>
+    </div>
+</nav>
+
+`;
