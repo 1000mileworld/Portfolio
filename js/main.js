5bf317bd1caa100a7402b55dad6c4997a413c484
Joe Liang <56658669+1000mileworld@users.noreply.github.com>
2020-03-28 17:53:41 -0400

    customized slideshow nav arrows

--- js/main.js
+++ js/main.js
@@ -3,7 +3,19 @@ $(document).ready(function(){
         $(this).toggleClass("pulse");
     });
     $(".intro__contact").hover(function(){
-        $("#arrow").toggleClass("slide-right")
-    })
+        $("#arrow").toggleClass("slide-right");
+    });
+
+    //Project.html
+    $(".carousel-control.left").hover(function(){
+        $(this).toggleClass("control-hover");
+        $(".arrow.left").toggleClass("arrow-hover");
+    });
+
+    $(".carousel-control.right").hover(function(){
+        $(this).toggleClass("control-hover");
+        $(".arrow.right").toggleClass("arrow-hover");
+    });
 });
 
+//.carousel-control.left
